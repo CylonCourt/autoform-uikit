6eafb9a2f953b08439253d5e6bbcb7d7aa90aee1
David Jenniex <d.jenniex@terein.com>
2015-12-28 17:47:53 -0700

    Specify version of ecmascript package

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.use([
-    'ecmascript',
+    'ecmascript@0.1.6',
     'templating',
     'underscore',
   ], 'client');
